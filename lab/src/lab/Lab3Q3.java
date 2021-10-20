3a1c47eecafe163f76eb227614d72345d3271da7
SOHTA0318 <nicosohta0318@gmail.com>
2021-10-20 14:56:28 -0700

    delete some

--- lab/src/lab/Lab3Q3.java
+++ lab/src/lab/Lab3Q3.java
@@ -6,21 +6,20 @@ import java.util.List;
 
 public class Lab3Q3 {
 public static void main(String[] args){
-	List<Integer> Year = new ArrayList<>();
-	final var car1 = new Car("Honda", "Civic",1999);
-	final var car2 = new Car("Toyota", "Civic",2005);
-	final var car3 = new Car("Prius", "Civic",1865);
-	final var car4 = new Car("Nissan", "Civic",2010);
-	final var car5 = new Car("BMW", "Civic",1923);
-	 
-	Year.add(car1.year());
-	Year.add(car2.year());
-	Year.add(car3.year());
-	Year.add(car4.year());
-	Year.add(car5.year());
-	
-	Collections.sort(Year);
-	System.out.println(Year);
+	ArrayList <Car>list = new ArrayList<Car>();
+    list.add(new Car("Honda", "Civic",1999));
+    list.add(new Car("Toyota", "Civic",2005));
+    list.add(new Car("Prius", "Civic",1865));
+    list.add(new Car("Nissan", "Civic",2010));
+    list.add(new Car("BMW", "Civic",1923));
+
+    Collections.sort(list);
+    for (Car carNumber: list) {
+        System.out.print("[Car Year = " + carNumber.year());
+        System.out.println(", Make = " + carNumber.make());
+        System.out.println(", Model = " + carNumber.model());
+    }
+}
 	
 	
 	
@@ -29,8 +28,33 @@ public static void main(String[] args){
 
 
 
-}
-record Car(String make,String model, int year) {
+class Car implements Comparable<Car> {
+    private String make;
+    private String model;
+    private int year;
+    public Car(String make,String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+  
+	public int year() {
+        return this.year;
+    }
+    public String make() {
+        return this.make;
+    }
+    public String model() {
+        return this.model;
+    }
+    public int compareTo1( Car car ){
+        return this.year() - car.year();
+    }
 
-}
+	@Override
+	public int compareTo(Car o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+} 
 
